b713b39b650fd0af503c70e28f3254fc0ff5c1c9
Yagyesh Bobde <yagyeshsrbobde@gmail.com>
2023-02-14 07:54:25 +0530

    added the video stream and video grid

--- public/script.js
+++ public/script.js
@@ -1,14 +1,38 @@
 const socket = io('/')
 
-const myPeer = new Peer(undefined , {
-    host : '/',
-    port : '3001'
+const videoGrid = document.getElementById('video-grid')
+
+
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
 })
+
+const myVideo = document.createElement('video')
+myVideo.muted = true
+myVideo.autoplay = true
+
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)
+})
+
 
 myPeer.on('open' , id => {
     socket.emit('join-room' , ROOM_ID, id)
 })
 
+
+
 socket.on('user-connected' , userId => {
     console.log("User connected: " + userId)
 } )
+
+
+function addVideoStream(video , stream) {
+    video.srcObject = stream;
+    videoGrid.append(video)
+}
